c4caf351efe9731aa4dd2cb5c4952d270a9a0cf6
Chris Latham <cl@loyaltyangels.com>
2016-06-28 12:08:17 +0100

    fix test packages

--- manage.py
+++ manage.py
@@ -1,12 +1,30 @@
+#!/usr/bin/env python
 import os
+from flask.ext.script import Manager, Shell, Server
 from app import create_app
-from flask.ext.script import Manager
+import settings
 
 app = create_app()
+app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 manager = Manager(app)
 
+# access python shell with context
+manager.add_command("shell", Shell(make_context=lambda: {'app': app}), use_ipython=True)
+
+# run the app
+manager.add_command("runserver", Server(port=settings.DEV_PORT, host=settings.DEV_HOST, threaded=True))
+
 HERE = os.path.abspath(os.path.dirname(__file__))
-UNIT_TEST_PATH = os.path.join(HERE, 'app', 'tests', 'unit')
+UNIT_TEST_PATH = os.path.join(HERE, 'app', 'tests')
+
+
+@manager.command
+def test():
+    """Run the tests."""
+    import pytest
+    exit_code = pytest.main([UNIT_TEST_PATH, '--verbose'])
+    return exit_code
+
 
 if __name__ == '__main__':
     manager.run()
--- settings.py
+++ settings.py
@@ -1,6 +1,16 @@
 from environment import env_var, read_env
+import os
+
+SECRET_KEY = b'\x00\x8d\xab\x02\x88\\\xc2\x96&\x0b<2n0n\xc9\x19\xec8\xab\xc5\x08N['
 
 read_env()
+
+APP_DIR = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
+DEBUG = env_var("ZEUS_DEBUG", False)
+
+DEV_HOST = env_var("DEV_HOST", "0.0.0.0")
+DEV_PORT = env_var("DEV_PORT", "5050")
 
 TESTING = env_var("METIS_TESTING", False)
 
