6239062cd1d8e861ef3d43da130a1e09e4c9a988
Chris Latham <cl@bink.com>
2017-01-11 09:44:31 +0000

    mock scandir with pyfakefs ScanDir method

--- app/tests/unit/test_visa_handback_file_processor.py
+++ app/tests/unit/test_visa_handback_file_processor.py
@@ -1,27 +1,20 @@
 import os
-import mock
-from pyfakefs import fake_filesystem_unittest
-# The module under test is pyfakefs.visa_handback_file_processor
-from scandir import scandir
+from unittest.mock import patch
 
+from pyfakefs import fake_filesystem_unittest
+
+# The module under test is pyfakefs.visa_handback_file_processor
 import settings
 from app.visa_handback_file_processor import get_dir_contents, mkdir_p
-
-
-def scandir_function(path=''):
-    return ['./']
 
 
 class TestVisaHandback(fake_filesystem_unittest.TestCase):
     def setUp(self):
         self.setUpPyfakefs()
 
-    def tearDown(self):
-        # It is no longer necessary to add self.tearDownPyfakefs()
-        pass
-
-    @mock.patch('app.visa_handback_file_processor.scandir', side_effect=scandir)
-    def test_get_dir_contents(self, scandir_function):
+    @patch('app.visa_handback_file_processor.scandir')
+    def test_get_dir_contents(self, mock_scandir):
+        mock_scandir.side_effect = self.fs.ScanDir
         '''Test visa_handback_file_processor.dir_exists()'''
         # The os module has been replaced with the fake os module so all of the
         # following occurs in the fake filesystem.
@@ -32,4 +25,6 @@ class TestVisaHandback(fake_filesystem_unittest.TestCase):
         with open(touched_file, 'a'):
             os.utime(touched_file, None)
         payment_files = get_dir_contents('../../' + settings.VISA_SOURCE_FILES_DIR)
-        self.assertTrue(len(payment_files))
+        self.assertIsInstance(payment_files, list)
+        self.assertEqual(len(payment_files), 1)
+        self.assertTrue(payment_files[0].endswith('/afile.txt'))
