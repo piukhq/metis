de0f895f13802a90bd65d96600ac0d0a2287ab0c
Chris Latham <cl@bink.com>
2017-01-17 14:27:18 +0000

    add test

--- app/hermes.py
+++ app/hermes.py
@@ -4,7 +4,5 @@ from settings import HERMES_URL
 
 
 def provider_status_mapping(slug):
-    status_mapping = requests.get('{}/payment_cards/provider_status_mapping/{}'.format(
-        HERMES_URL,
-        slug)).json()
+    status_mapping = requests.get('{}/payment_cards/provider_status_mapping/{}'.format(HERMES_URL, slug)).json()
     return {x['provider_status']: x['bink_status'] for x in status_mapping}
