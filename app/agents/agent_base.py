2bb381bc149b9ddd97bdac5e6bfb61ef68513308
Chris Latham <cl@bink.com>
2017-01-19 16:39:41 +0000

    remove unused variable

--- app/agents/agent_base.py
+++ app/agents/agent_base.py
@@ -4,10 +4,10 @@ import settings
 # Username and password from Spreedly site - Loyalty Angels environments
 username = 'Yc7xn3gDP73PPOQLEB2BYpv31EV' if settings.TESTING else '1Lf7DiKgkcx5Anw7QxWdDxaKtTa'
 password = '94iV3Iyvky86avhdjLgIh0z9IFeB0pw4cZvu64ufRgaur46mTM4xepsPDOdxVH51'
-receiver_base_url = settings.SPREEDLY_RECEIVER_URL
 
 
 class AgentBase:
+
     @staticmethod
     def post_request(url, header, request_data):
         settings.logger.info('POST Spreedly request to {}'.format(url))
