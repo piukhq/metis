f7cb723c4d91c2f4b436da347febeaf3c9a5e26f
olivere <oe@bink.com>
2017-02-07 08:34:49 +0000

    Add request_header test case.

--- app/tests/unit/test_amex.py
+++ app/tests/unit/test_amex.py
@@ -1,4 +1,7 @@
-from unittest import TestCase
+from unittest import TestCase, mock
+import httpretty
+import json
+
 import app.agents.amex as amex
 
 
@@ -10,9 +13,38 @@ class TestAmex(TestCase):
         result = self.amex.add_url()
         self.assertEqual('https://api.qa.americanexpress.com/v3/smartoffers/sync', result)
 
+        result = self.amex.remove_url()
+        self.assertEqual('https://api.qa.americanexpress.com/v3/smartoffers/unsync', result)
+
     def test_receiver_token_testing(self):
         result = self.amex.receiver_token()
         self.assertIn('BqfFb1WnOwpbzH7WVTqmvYtffPV', result)
+
+    def amex_route(self):
+        auth_url = '{}{}'.format(amex.AMEX_URL, "/apiplatform/v2/oauth/token/mac")
+        payload = "grant_type=client_credentials&scope="
+
+        header = {"Content-Type": "application/x-www-form-urlencoded",
+                  "Authentication": amex.mac_auth_header(),
+                  "X-AMEX-API-KEY": amex.client_id}
+
+        httpretty.register_uri(httpretty.POST,
+                               '{}'.format(auth_url),
+                               status=200,
+                               headers=header,
+                               body=payload,
+                               content_type="application/x-www-form-urlencoded")
+
+    @mock.patch('json.loads')
+    @httpretty.activate
+    def test_request_header(self, mock_loads):
+        mock_loads.return_value = {"access_token": "1234567890", "mac_key": "99",}
+        self.amex_route()
+        result = self.amex.request_header(amex.res_path_sync)
+        self.assertIn(result[:6], '<![CDATA[')
+        self.assertIn(result[9:39], 'Content-Type: application/json')
+        self.assertIn(result[40:55], 'Authorization: ')
+        self.assertIn(result[169:219], 'X-AMEX-API-KEY: 91d207ec-267f-469f-97b2-883d4cfce44d')
 
     def test_request_body_correct_text(self):
         card_info = {'partner_slug': 'amex',
