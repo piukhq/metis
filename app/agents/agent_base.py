3d92b5ae3385340292fddbb3c207ab25b0bb9dea
mlatham <ml@bink.com>
2017-03-17 16:08:50 +0000

    New api key added and endpoint for setting cards to retained on Spreedly.

--- app/agents/agent_base.py
+++ app/agents/agent_base.py
@@ -3,7 +3,7 @@ import settings
 
 # Username and password from Spreedly site - Loyalty Angels environments
 username = 'Yc7xn3gDP73PPOQLEB2BYpv31EV' if settings.TESTING else '1Lf7DiKgkcx5Anw7QxWdDxaKtTa'
-password = '94iV3Iyvky86avhdjLgIh0z9IFeB0pw4cZvu64ufRgaur46mTM4xepsPDOdxVH51'
+password = '4m8tVKWHvakjhfBXnRVDbfkOArB9NvAyo9U6lWtVulb2thuI6T439blRbQWGwQcH'
 
 
 class AgentBase:
