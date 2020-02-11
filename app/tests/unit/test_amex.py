8b97446117b53089d390b67ca1dfd6575507a7a8
kaziz <kaziz@bink.com>
2020-02-11 16:08:09 +0000

    fix unit tests for amex

--- app/tests/unit/test_amex.py
+++ app/tests/unit/test_amex.py
@@ -8,12 +8,12 @@ import app.agents.amex as amex
 
 class TestAmex(TestCase):
     def setUp(self):
+        settings.TESTING = True
         self.amex = amex.Amex()
 
         self.card_info = {'partner_slug': 'amex',
                           'payment_token': '3ERtq3pUV5OiNpdTCuhhXLBmnv8',
                           'card_token': ''}
-        settings.TESTING = True
 
     def tearDown(self):
         settings.TESTING = False
@@ -23,8 +23,8 @@ class TestAmex(TestCase):
         payload = "grant_type=client_credentials&scope="
 
         header = {"Content-Type": "application/x-www-form-urlencoded",
-                  "Authentication": amex.mac_auth_header(),
-                  "X-AMEX-API-KEY": amex.client_id}
+                  "Authentication": self.amex.mac_auth_header(),
+                  "X-AMEX-API-KEY": self.amex.client_id}
 
         httpretty.register_uri(httpretty.POST,
                                '{}'.format(auth_url),
@@ -42,7 +42,7 @@ class TestAmex(TestCase):
         self.assertIn(result[:6], '<![CDATA[')
         self.assertIn(result[9:39], 'Content-Type: application/json')
         self.assertIn(result[40:55], 'Authorization: ')
-        self.assertIn(result[169:219], 'X-AMEX-API-KEY: 91d207ec-267f-469f-97b2-883d4cfce44d')
+        self.assertIn(result[169:219], f'X-AMEX-API-KEY: {self.amex.client_id}')
 
     def test_remove_card_request_body(self):
         result = self.amex.remove_card_request_body(self.card_info)
@@ -59,7 +59,7 @@ class TestAmex(TestCase):
         self.assertIn('cmAlias1', result)
 
     def test_mac_auth_header(self):
-        result = amex.mac_auth_header()
+        result = self.amex.mac_auth_header()
         self.assertIn('MAC id', result)
 
     @mock.patch('json.loads')
