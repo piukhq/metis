287100a3a4a9e0d62b417c6123a2798c785bf7b2
mmarsh <mmarsh@bink.com>
2020-04-28 17:57:18 +0100

    some code refactoring and url corrections

--- app/services.py
+++ app/services.py
@@ -69,6 +69,13 @@ def post_request(url, header, request_data):
     return resp
 
 
+def put_request(url, header, request_data):
+    settings.logger.info('PUT Spreedly Request to URL: {}'.format(url))
+    resp = requests.put(url, auth=(username, password), headers=header, data=request_data)
+    settings.logger.info('Spreedly PUT response: {}'.format(resp.text))
+    return resp
+
+
 def add_card(card_info):
     """Once the receiver has been created and token sent back, we can pass in card details, without PAN.
     Receiver_tokens kept in settings.py."""
@@ -113,20 +120,46 @@ def remove_card(card_info):
     settings.logger.info('Start Remove card for {}'.format(card_info['partner_slug']))
 
     agent_instance = get_agent(card_info['partner_slug'])
-
     header = agent_instance.header
-    # 'https://core.spreedly.com/v1/receivers/' + agent_instance.receiver_token()
-    url = '{}/receivers/{}'.format(settings.SPREEDLY_BASE_URL, agent_instance.receiver_token())
 
-    try:
-        request_data = agent_instance.remove_card_body(card_info)
-    except OAuthError:
-        # 5 = PROVIDER_SERVER_DOWN
-        # TODO: get this from gaia
-        put_account_status(5, card_id=card_info['id'])
-        return None
+    if card_info['partner_slug'] == 'visa':
+        # Note the other agents use Spreedly as a Proxy which may need to be changed at some stage by adding to each
+        # agent_instance an un_enroll method and removing remove_card_body(card_info)
+        # For VOP we will un-enroll directly with VOP and not use Spreedly as a Proxy, however, a new Spreedly call will
+        # be made to redact the Payment method.
+        # VOP Un-enroll
+        success, resp = agent_instance.un_enroll(card_info)
+        if not success:
+            settings.logger.info(
+                f"Un-enroll failed for card {card_info['partner_slug']} account id {card_info['id']}"
+            )
+        # if the unroll fails it does not need to be retied but we will continue to remove card from spreedly
+        url = f'{settings.SPREEDLY_BASE_URL}/v1/payment_methods/{agent_instance.receiver_token}/redact.json'
 
-    resp = post_request(url, header, request_data)
+        redacted = False
+        try:
+            redact_resp = put_request(url, header, "")
+            if redact_resp.status_code < 300:
+                redacted = True
+        except OAuthError:
+            pass
+        if not redacted:
+            settings.logger.info(
+                f"Un-enroll Spreedly redact failed for card {card_info['partner_slug']} account id {card_info['id']}"
+            )
+    else:
+        # Older call used with Agents prior to VOP which proxy through Spreedly
+        # 'https://core.spreedly.com/v1/receivers/' + agent_instance.receiver_token()
+        url = '{}/receivers/{}'.format(settings.SPREEDLY_BASE_URL, agent_instance.receiver_token())
+
+        try:
+            request_data = agent_instance.remove_card_body(card_info)
+        except OAuthError:
+            # 5 = PROVIDER_SERVER_DOWN
+            # TODO: get this from gaia
+            put_account_status(5, card_id=card_info['id'])
+            return None
+        resp = post_request(url, header, request_data)
 
     # get the status mapping for this provider from hermes.
     status_mapping = get_provider_status_mappings(card_info['partner_slug'])
