9e5cabb217d3f9445f0f5f7580abb8d906bfd31f
kaziz <kaziz@bink.com>
2020-08-24 16:32:08 +0100

    Update amex tests

--- app/tests/unit/test_amex.py
+++ app/tests/unit/test_amex.py
@@ -9,6 +9,7 @@ import app.agents.amex as amex
 class TestAmex(TestCase):
     def setUp(self):
         settings.TESTING = True
+        settings.STUBBED_AMEX_URL = "http://localhost"
         self.amex = amex.Amex()
 
         self.card_info = {'partner_slug': 'amex',
