efeb223861ea07c8ddc3b7dd04f495dd7ae68cc6
Francesco Milani <fmilani@bink.com>
2020-09-17 14:25:16 +0300

    remove unused change

--- app/agents/agent_base.py
+++ app/agents/agent_base.py
@@ -1,9 +1,8 @@
-# Username and password from Spreedly site - Loyalty Angels environments
 from app.services import send_request
 
 
 class AgentBase:
 
-    @classmethod
-    def post_request(cls, url, header, request_data):
+    @staticmethod
+    def post_request(url, header, request_data):
         return send_request('POST', url, header, request_data)
