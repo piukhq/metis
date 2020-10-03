39ebc77ad4a42f066ec7dad2fa26bd53502ae82a
mmarsh <mmarsh@bink.com>
2020-10-03 14:00:09 +0100

    Vop enrol test case

--- app/tests/unit/test_visa_offers.py
+++ app/tests/unit/test_visa_offers.py
@@ -7,7 +7,7 @@ import httpretty
 import settings
 from app.agents.visa_offers import Visa
 from app.action import ActionCode
-from app.services import remove_card
+from app.services import remove_card, add_card, get_spreedly_url
 from copy import copy
 
 
@@ -163,6 +163,119 @@ class VOPUnenroll(unittest.TestCase):
         self.assertEqual("Delete", req["response_action"])
         self.assertEqual([], req["deactivated_list"])
         self.assertEqual(2, len(req["deactivate_errors"]))
+
+
+class VOPEnroll(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        settings.TESTING = True
+        settings.VOP_SPREEDLY_BASE_URL = "http://localhost:8006"
+        cls.visa = Visa()
+        cls.vop_enrol_url = f"{cls.visa.vop_url}{cls.visa.vop_enrol}"
+        cls.hermes_payment_card_call_back = f"{settings.HERMES_URL}/payment_cards/accounts/status"
+        cls.hermes_status_mapping = f"{settings.HERMES_URL}/payment_cards/provider_status_mappings/visa"
+
+        cls.card_info = {
+            "partner_slug": "visa",
+            "payment_token": "psp_token",
+            'card_token': "card_token",
+            'id': 1234,
+            'date': arrow.now().timestamp,
+            "action_code": ActionCode.ADD,
+            "retry_id": -1
+        }
+
+    def register_success_requests(self, card_info):
+        body_dict = {
+            "userDetails": {
+                "externalUserId": "a74hd93d9812wir0174mk093dkie1",
+                "communityCode": "BINKCTE01",
+                "userId": "809902ef-3c0b-40b8-93bf-63e2621df06f",
+                "userKey": "a74hd93d9812wir0174mk093dkie1",
+                "languageId": "en-US",
+                "timeZoneId": "Pacific Standard Time",
+                "timeZoneShortCode": "PST",
+                "cards": [
+                    {
+                        "cardId": "bfc33c1d-d4ef-e111-8d48-001a4bcdeef4",
+                        "cardLast4": "1111",
+                        "productId": "A",
+                        "productIdDescription": "Visa Traditional",
+                        "productTypeCategory": "Credit",
+                        "cardStatus": "New"
+                    }
+                ],
+                "userStatus": 1,
+                "enrollDateTime": "2020-01-29T15:02:55.067"
+            },
+            "correlationId": "ce708e6a-fd5f-48cc-b9ff-ce518a6fda1a",
+            "responseDateTime": "2020-01-29T15:02:55.1860039Z",
+            "responseStatus": {
+                "code": "SUCCESS",
+                "message": "Request proceed successfully without error.",
+                "responseStatusDetails": []
+            }
+        }
+        self.register_requests(
+            {
+                "transaction": {
+                    "response": {
+                        "status": 200,
+                        "body": json.dumps(body_dict)
+                    }
+                }
+            }, card_info)
+
+    def register_requests(self, vop_response, card_info):
+        spreedly_url = f"{get_spreedly_url(card_info['partner_slug'])}/receivers/{self.visa.receiver_token()}"
+        print(f"Mocking out {spreedly_url}")
+        httpretty.register_uri(
+            httpretty.POST,
+            spreedly_url,
+            body=json.dumps(vop_response)
+        )
+        print(f"Mocking out call back {self.hermes_payment_card_call_back}")
+        httpretty.register_uri(
+            httpretty.PUT,
+            self.hermes_payment_card_call_back,
+            body=''
+        )
+        print(f"Mocking out error mapping call back {self.hermes_payment_card_call_back}")
+        httpretty.register_uri(
+            httpretty.GET,
+            self.hermes_status_mapping,
+            body=json.dumps(
+                [
+                    {"provider_status_code": "Delete:1000", "bink_status_code": 10},
+                    {"provider_status_code": "Delete:4000", "bink_status_code": 20}
+                ]
+            )
+        )
+
+        print("End points mocked")
+
+    def assert_success(self):
+        request = httpretty.last_request()
+        expected = {"id": 1234, "response_state": "Success", "response_status": "Add:SUCCESS",
+                    "response_message": "Request proceed successfully without error.;", "response_action": "Add",
+                    "retry_id": -1
+                    }
+        actual = json.loads(request.body)
+        self.assertDictEqual(expected, actual)
+
+    @httpretty.activate
+    def test_add_card_success(self):
+        self.register_success_requests(self.card_info)
+        try:
+            add_card(self.card_info)
+        except Exception as e:
+            self.assertFalse(True, msg=f"Exception: {e}")
+        prev_req = httpretty.latest_requests()
+        for req in prev_req:
+            print(req.body)
+
+        ssert_success
+
 
 
 if __name__ == '__main__':
