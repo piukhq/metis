e3c8a428418fa2b01b3a1c1c014cd3b9192747a2
Tony Truong <ttruong@bink.com>
2021-04-16 09:27:55 +0100

    vop monitoring for enrolments

--- prometheus/metrics.py
+++ prometheus/metrics.py
@@ -10,14 +10,14 @@ registry = CollectorRegistry()
 status_counter = Counter(
     name="retain_status_by_provider",
     documentation="Count for retain status by provider.",
-    labelnames=("provider", "status",),
+    labelnames=("provider", "status", ),
     namespace=NAMESPACE,
 )
 
 spreedly_retain_processing_seconds_histogram = Histogram(
     name="spreedly_retain_processing_seconds_histogram",
     documentation="Response time for Spreedly retain.",
-    labelnames=("provider", "status"),
+    labelnames=("provider", "status", ),
     buckets=(5.0, 10.0, 30.0, 300.0, 3600.0, 43200.0, 86400.0, float("inf")),
     namespace=NAMESPACE,
 )
